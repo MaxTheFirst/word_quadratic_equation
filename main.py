c1c94a507c07fb733f1e29a375ee79a8f6ab19b4
MaxTheFirst <maks.vershinin.2005@mail.ru>
2023-08-30 21:41:36 +0300

    v0.0.1

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from win32com import __gen_path__
 import win32com.client as win32
 from calc import get_decision
+from os import walk
+from shutil import rmtree
 
 
 def get_selections():
-    word = win32.gencache.EnsureDispatch('Word.Application')
-    doc = word.ActiveDocument
-    return doc, word.Selection
+    try:
+        word = win32.gencache.EnsureDispatch('Word.Application')
+        word.ActiveDocument
+    except AttributeError:
+        paths = list(walk(__gen_path__))
+        rmtree(paths[0][0] + '\\' + paths[0][1][0])
+        return get_selections()
+    return word.Selection
 
 
 def main():
